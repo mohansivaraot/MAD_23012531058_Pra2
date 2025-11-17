a613574b656a8201244cf479f7ac75f1b41e223a
MOHAN SIVA RAO <mohansivaraot@gmail.com>
2025-11-17 21:21:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_23012531058_pra2/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.mad_23012531058_pra2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private val tag = "mainActivityLifecycle"
+
+    private lateinit var mainLayout: ConstraintLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun logShow (message: String) { Log.d(tag, message)
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        Snackbar.make(mainLayout, message, Snackbar.LENGTH_SHORT).show()
+    }
+    override fun onStart() {
+        super.onStart()
+        logShow("onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        logShow("onResume")
+}
+
+    override fun onPause() {
+        super.onPause()
+        logShow("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        logShow("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        logShow("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        logShow("onDestroy")
+    }
+}
